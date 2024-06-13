de6cdf4a58426087c0cce950545d2b73c7a23d15
Dihan Khan <121043757+dihands@users.noreply.github.com>
2024-06-13 13:19:30 -0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -3,30 +3,37 @@ function handleSubmit(event) {
     
     // Get form values
     const fullName = document.getElementById('fullName').value;
+
     const password = document.getElementById('password').value;
     
     // Store data in localStorage (for demonstration purposes)
     localStorage.setItem('fullName', fullName);
+
     localStorage.setItem('password', password);
     
     // Redirect to profile page
-    window.location.href = 'profil.html';
+    window.location.href = 'profi.html';
 }
 
 // Display user details on profile.html
 window.onload = function() {
     const fullName = localStorage.getItem('fullName');
+
     const password = localStorage.getItem('password');
 
-    if (fullName && password) {
+    
+    if (fullName && email) {
         const profileDetails = document.getElementById('profileDetails');
         profileDetails.innerHTML = `
             <p><strong>Name:</strong> ${fullName}</p>
+
             <p><strong>Password:</strong> ${password}</p>
         `;
         
         // Optional: Clear localStorage after displaying details
         // localStorage.removeItem('fullName');
-        // localStorage.removeItem('password');
+        // localStorage.removeItem('email');
     }
 }
+
+
